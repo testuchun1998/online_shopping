2d5d5eb8a520daedb3c073b2417357e8ccb40a83
MuhammadiBoboyev <boboyevmuhammadi98@gmail.com>
2022-09-01 17:44:24 +0500

    boshliq_aytgani_ochirildi

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -12,13 +12,5 @@ public class Main {
         System.out.println("salom java");
         System.out.println("salom java");
 
-
-        System.out.println("qalisan o'gayni");
-        System.out.println("qalisan o'gayni");
-        System.out.println("qalisan o'gayni");
-        System.out.println("qalisan o'gayni");
-        System.out.println("qalisan o'gayni");
-        System.out.println("qalisan o'gayni");
-
     }
 }
