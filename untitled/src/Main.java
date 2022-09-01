183f135307bd8bf671a05ea9f3fd70cd16ef7533
MuhammadiBoboyev <boboyevmuhammadi98@gmail.com>
2022-09-01 17:31:27 +0500

    change_one

---
+++ untitled/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+        System.out.println("salom java");
+
+    }
+}
